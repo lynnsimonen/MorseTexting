6e95368ef13e2b146f67c066b79d26bd9e60bdfc
Lynn Simonen <lynnsimonen@yahoo.com>
2022-10-27 19:47:30 -0500

    Add project files.

---
+++ MorseTexting/MorseTexting/MainPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System.Linq;
+using Xamarin.Forms;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace MorseTexting
+{
+    public partial class MainPage : ContentPage
+    {
+        public string dotsAndDashesUpper { get; set; }
+        public string dotsAndDashesLower { get; set; }
+        public string spaceBarUpper { get; set; }
+        public string spaceBarLower { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            dotsAndDashesUpper = "";
+            spaceBarUpper = "";
+            dotsAndDashesLower = "";
+            spaceBarLower = "";
+        }
+        
+        void Btn_UpperDotClick(object sender, System.EventArgs e)
+        {
+            dotsAndDashesUpper += ".";
+            lbl4_UpperMorse.Text = dotsAndDashesUpper;
+        }
+
+        void Btn_UpperDashClick(object sender, System.EventArgs e)
+        {
+            dotsAndDashesUpper += "-";
+            lbl4_UpperMorse.Text = dotsAndDashesUpper;
+        }
+
+        void Btn_UpperSpaceBarClick(object sender, System.EventArgs e)
+        {
+            if (dotsAndDashesUpper.Length != 0)
+            {
+                lbl4_UpperTextScreen.Text += MorseAlpha.DeCoder(dotsAndDashesUpper);
+                dotsAndDashesUpper = "";
+                lbl4_UpperMorse.Text = dotsAndDashesUpper;
+            }
+            else
+            {
+                lbl4_UpperTextScreen.Text += " ";
+                dotsAndDashesUpper = "";
+            }
+
+        }
+
+        //----------------------------------------------
+
+        void Btn_LowerDotClick(object sender, System.EventArgs e)
+        {
+            dotsAndDashesLower += ".";
+            lbl6_LowerMorse.Text = dotsAndDashesLower;
+        }
+
+        void Btn_LowerDashClick(object sender, System.EventArgs e)
+        {
+            dotsAndDashesLower += "-";
+            lbl6_LowerMorse.Text = dotsAndDashesLower;
+        }
+
+        void Btn_LowerSpaceBarClick(object sender, System.EventArgs e)
+        {
+            if (dotsAndDashesLower.Length != 0)
+            {
+                lbl6_LowerTextScreen.Text += MorseAlpha.DeCoder(dotsAndDashesLower);
+                dotsAndDashesLower = "";
+                lbl6_LowerMorse.Text = dotsAndDashesLower;
+            }
+            else
+            {
+                lbl6_LowerTextScreen.Text += " ";
+                dotsAndDashesLower = "";
+            }
+        }
+
+        
+    }
+}
---
+++ MorseTexting/MorseTexting/Morse.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MorseTexting
+{
+    public class MorseAlpha
+    {
+        private static string[] alphaLetters { get; set; }
+        private static string[] morseCode { get; set; }
+        private static string morse { get; set; }
+        public MorseAlpha()
+        {
+
+        }
+
+
+        public static string DeCoder(string morse)
+        {
+            alphaLetters = new[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+
+            morseCode = new[] { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+
+            int indexCode;
+                     
+          
+            indexCode = Array.IndexOf(morseCode, morse);
+            if (indexCode != -1)
+            {
+                string letter = (string)alphaLetters.GetValue(indexCode);
+                return letter;
+            }
+            else
+            { 
+                return ""; 
+            }          
+            
+        }
+    }
+}
